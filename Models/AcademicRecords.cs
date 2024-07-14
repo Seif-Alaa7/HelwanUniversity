6ff3f713263192c0b740f89d03eb35a1da0d680b
seifalaa <seifa@DESKTOP-CJMO4KL>
2024-07-14 21:37:24 +0300

    Add Precision to decimal columns

--- Models/AcademicRecords.cs
+++ Models/AcademicRecords.cs
@@ -1,6 +1,7 @@
 ﻿using Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,15 +10,40 @@ namespace Models
 {
     public class AcademicRecords
     {
+        private decimal _GPASemester;
+        private decimal _GPATotal;
+        private decimal _SemesterPoints;
+        private decimal _TotalPoints;
+
         public int Id { get; set; }
         public int StudentId { get; set; }
-        public decimal GPASemester { get; private set; }
-        public decimal GPATotal { get; private set; }
+        [Column(TypeName = "decimal(18, 3)")]
+        public decimal GPASemester
+        {
+            get => _GPASemester;
+            set => _GPASemester = Math.Round(value, 3);
+        }
+        [Column(TypeName = "decimal(18, 3)")]
+        public decimal GPATotal
+        {
+            get => _GPATotal;
+            set => _GPATotal = Math.Round(value, 3);
+        }
         public int CreditHours { get; private set; }
         public int RecordedHours { get; private set; }
         public int TotalHours { get; private set; }
-        public decimal SemesterPoints { get; private set; }
-        public decimal TotalPoints { get; private set; }
+        [Column(TypeName = "decimal(18, 3)")]
+        public decimal SemesterPoints
+        {
+            get => _SemesterPoints;
+            set => _SemesterPoints = Math.Round(value, 3);
+        }
+        [Column(TypeName = "decimal(18, 3)")]
+        public decimal TotalPoints
+        {
+            get => _TotalPoints;
+            set => _TotalPoints = Math.Round(value, 3);
+        }
         public Level Level { get; private set; }
         public Semester Semester { get; private set; }
         public Student Student { get; set; } = null!;
--- Models/Student Subjects.cs
+++ Models/Student Subjects.cs
@@ -1,6 +1,7 @@
 ﻿using Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,18 @@ namespace Models
 {
     public class StudentSubjects
     {
+        private decimal _DegreePoints;
+
         public int StudentId { get; set; }
         public int SubjectId { get; set; }
         public int? Degree { get; set; }
         public Grade? Grade { get; private set; }
-        public decimal DegreePoints { get; private set; }
+        [Column(TypeName = "decimal(18, 3)")]
+        public decimal DegreePoints
+        {
+            get => _DegreePoints;
+            set => _DegreePoints = Math.Round(value, 3);
+        }
         public Student Student { get; set; } = null!;
         public Subject Subject { get; set; } = null!;
 
