da78c85404200bda5bda1a72e1941916a433a6dd
AyaEsmaeel <115806612+AyaEsmaeel@users.noreply.github.com>
2024-07-27 16:17:28 +0300

    add FrontEnd

--- HelwanUniversity/wwwroot/js/site.js
+++ HelwanUniversity/wwwroot/js/site.js
@@ -1,4 +1,58 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿        document.addEventListener('DOMContentLoaded', function () {
+            const loader = document.querySelector('.loader-container');
+            const header = document.querySelector('header');
+            const footer = document.querySelector('footer');
 
-// Write your JavaScript code.
+            setTimeout(function () {
+                loader.style.display = 'none';
+                header.style.display = 'block';
+                footer.style.display = 'block';
+            }, 3000); 
+        });
+        document.addEventListener('DOMContentLoaded', function () {
+            const navbar = document.querySelector('.navbar');
+
+            function handleScroll() {
+                if (window.scrollY > 50) {
+                    navbar.classList.add('scrolled');
+                } else {
+                    navbar.classList.remove('scrolled');
+                }
+            }
+
+            window.addEventListener('scroll', handleScroll);
+
+            handleScroll();
+        });
+        document.addEventListener("DOMContentLoaded", function () {
+            var img = new Image();
+            img.src = '/img9.png';
+            img.onload = function () {
+                document.querySelector('.header').classList.add('loaded');
+            };
+        });
+
+        // script.js
+
+        document.addEventListener('DOMContentLoaded', () => {
+            const toggle = document.getElementById('theme-toggle');
+            const body = document.body;
+
+            if (localStorage.getItem('theme') === 'dark') {
+                body.classList.add('dark-theme');
+                toggle.classList.replace('fa-sun', 'fa-moon');
+            }
+
+            toggle.addEventListener('click', () => {
+                body.classList.toggle('dark-theme');
+
+                if (body.classList.contains('dark-theme')) {
+                    localStorage.setItem('theme', 'dark');
+                    toggle.classList.replace('fa-sun', 'fa-moon');
+                } else {
+                    localStorage.setItem('theme', 'light');
+                    toggle.classList.replace('fa-moon', 'fa-sun');
+                }
+            });
+        });
+
