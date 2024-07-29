259b4079140f9b5e554ac591156a462a5f998ef7
seifalaa <seifa@DESKTOP-CJMO4KL>
2024-07-29 20:11:35 +0300

    Create Doctor , Highboared , Student Controllers

--- HelwanUniversity/Controllers/DoctorController.cs
+++ HelwanUniversity/Controllers/DoctorController.cs
@@ -1,12 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.Repository;
+using Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Models.Enums;
+using System.Net;
+using ViewModels;
 
 namespace HelwanUniversity.Controllers
 {
     public class DoctorController : Controller
     {
+        private readonly IDoctorRepository doctorRepository;
+
+        public DoctorController(IDoctorRepository doctorRepository)
+        {
+            this.doctorRepository = doctorRepository;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Details(int id)
+        {
+            var DoctorDatails = doctorRepository.GetOne(id);
+            return View(DoctorDatails);
+        }
+        public IActionResult Edit(int id)
+        {
+            var doctor = doctorRepository.GetOne(id);
+            var doctorVM = new DoctorVM
+            {
+                Id = id,
+                Name = doctor.Name,
+                Address = doctor.Address,
+                JobTitle = doctor.JobTitle,
+                Picture = doctor.Picture,
+                Gender = doctor.Gender,
+                Religion = doctor.Religion
+            };
+            return View(doctorVM);
+        }
+        [HttpPost]
+        public IActionResult SaveEdit(DoctorVM doctorVM)
+        {
+            var doctor = doctorRepository.GetOne(doctorVM.Id);
+
+            doctor.Id = doctorVM.Id;
+            doctor.Name = doctorVM.Name;
+            doctor.Address = doctorVM.Address;
+            doctor.JobTitle = doctorVM.JobTitle;
+            doctor.Picture = doctorVM.Picture;
+            doctor.Gender = doctorVM.Gender;
+            doctor.Religion = doctorVM.Religion;
+
+            doctorRepository.Update(doctor);
+            doctorRepository.Save();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Doctor doctor)
+        {
+            doctorRepository.Delete(doctor);
+            doctorRepository.Save();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- HelwanUniversity/Controllers/StudentController.cs
+++ HelwanUniversity/Controllers/StudentController.cs
@@ -1,12 +1,80 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.Repository;
+using Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using ViewModels;
 
 namespace HelwanUniversity.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly IStudentRepository studentRepository;
+
+        public StudentController(IStudentRepository studentRepository)
+        {
+            this.studentRepository = studentRepository;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Details(int id)
+        {
+            var studentDatails = studentRepository.GetOne(id);
+            return View(studentDatails);
+        }
+        public IActionResult Edit(int id)
+        {
+            var student = studentRepository.GetOne(id);
+            var studentVM = new StudentVM
+            {
+                Id = id,
+                Name = student.Name,
+                DepartmentId = student.DepartmentId,
+                Address = student.Address,
+                AdmissionDate = student.AdmissionDate,
+                BirthDate = student.BirthDate,
+                Gender = student.Gender,
+                Nationality = student.Nationality,
+                PaymentFees = student.PaymentFees,
+                PaymentFeesDate = student.PaymentFeesDate,
+                PhoneNumber = student.PhoneNumber,
+                Picture = student.Picture,
+                Religion = student.Religion
+            };
+            return View(studentVM);
+        }
+        [HttpPost]
+        public IActionResult SaveEdit(StudentVM studentVM)
+        {
+            var student = studentRepository.GetOne(studentVM.Id);
+
+            student.PhoneNumber = studentVM.PhoneNumber;
+            student.Address = studentVM.Address;
+            student.AdmissionDate = studentVM.AdmissionDate;
+            student.BirthDate = studentVM.BirthDate;
+            student.Name = studentVM.Name;
+            student.Picture = studentVM.Picture;
+            student.Gender = studentVM.Gender;
+            student.Religion = studentVM.Religion;
+            student.DepartmentId = studentVM.DepartmentId;
+            student.Nationality = studentVM.Nationality;
+            student.PaymentFees = studentVM.PaymentFees;
+            student.PaymentFeesDate = studentVM.PaymentFeesDate;
+            student.Id = studentVM.Id;
+
+            studentRepository.Update(student);
+            studentRepository.Save();
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Student student)
+        {
+            studentRepository.Delete(student);
+            studentRepository.Save();
+
+            return RedirectToAction("Index");
         }
     }
 }
