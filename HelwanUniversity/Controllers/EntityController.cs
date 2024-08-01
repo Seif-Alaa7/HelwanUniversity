ccf97e06d4b360c75ee8234623b668b9782cc014
seifalaa <seifa@DESKTOP-CJMO4KL>
2024-08-01 23:21:19 +0300

    solve some bugs

--- HelwanUniversity/Controllers/EntityController.cs
+++ HelwanUniversity/Controllers/EntityController.cs
@@ -50,12 +50,12 @@ namespace HelwanUniversity.Controllers
         }
         [HttpPost]
         public async Task<IActionResult> Add(AddEntity entity)
-        {
+        {/*
             if (!ModelState.IsValid)
             {
                 LoadPageData();
                 return View(entity);
-            }
+            }*/
             switch (entity.EntityType)
             {
 
